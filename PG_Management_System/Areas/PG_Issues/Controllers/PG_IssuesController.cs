f70c4a3439e846c53e08f5105bb24c0b21bead33
visha <visha@VISHAL>
2024-09-21 09:25:25 +0530

    add isssue page

---
+++ PG_Management_System/Areas/PG_Issues/Controllers/PG_IssuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PG_Management_System.Areas.PG_Issues.Controllers;
+
+[Area("PG_Issues")]
+[Route("PG_Issues/[controller]/[action]")]
+public class PG_IssuesController : Controller
+{
+    private IConfiguration _configuration;
+    public PG_IssuesController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    public IActionResult AllIssuesList()
+    {
+        return View();
+    }
+}
